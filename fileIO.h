fd748aa5a0f0804d3c1a1d768e8f0e2f36de28f9
Adil Khokhar <khokharadil@icloud.com>
2020-05-07 19:08:44 +0800

    Update fileIO.h

--- fileIO.h
+++ fileIO.h
@@ -5,6 +5,7 @@ void openFiles();
 int* readNextValue();
 void writeBuffer(buffer* buffer, int requestNo);
 void writeLift(lifts* lift);
+void writeResult(int movement, int requests);
 void closeFiles();
 
 #endif
